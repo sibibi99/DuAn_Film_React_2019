d4a5830f5725c49c36a55ea898399dca2cd3eb26
unknown <sibibi99@gmail.com>
2019-08-17 14:30:50 +0700

    remove cumrap

--- src/common/components/CumRap/CumRap.jsx
+++ src/common/components/CumRap/CumRap.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-// import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 import "../CumRap/CumRap.scss";
 import BHD from "../../../Assets/img/bhd.png";
@@ -7,43 +6,35 @@ import galaxycine from "../../../Assets/img/galaxycine.png";
 import megags from "../../../Assets/img/megags.png";
 import bt from "../../../Assets/img/bt.jpg";
 import dongdacinema from "../../../Assets/img/dongdacinema.png";
-import Rap from "./Rap/Rap";
-import { Tabs, Tab, TabPanel, TabList } from 'react-web-tabs';
+import { Tabs, Tab, TabPanel, TabList, TabProvider } from "react-web-tabs";
 
 export default function CumRap() {
   return (
     <section className="cumrap">
       <div className="cumrap__content">
         <h1 className="cumrap__title">CỤM RẠP</h1>
-
-        <Tabs forceRenderTabPanel defaultIndex={0}>
-          {/* Cap 1 - CUM RAP*/}
-          <TabList className="cumrap__icon">
-          <Tab className='cumrap__img'><img src={BHD} /></Tab>
-          <Tab className='cumrap__img'><img src={BHD} /></Tab>
-          <Tab className='cumrap__img'><img src={BHD} /></Tab>
-          <Tab className='cumrap__img'><img src={BHD} /></Tab>
-        </TabList>
-         
-          {/* Cap 2  - RAP PHIM*/}
-          <div className="cumrap__info">
-            <div className="cumrap__list">
-              <TabPanel className = "cumrap__panel" >
-              <Tabs forceRenderTabPanel className = "cumrap__menu">
-                <TabList className= "cumrap__ul">
-                  <Tab className= 'cumrap__li'><p>Day laf Tab 1</p></Tab>
-                  <Tab className= 'cumrap__li'><p>Day laf Tab 2</p></Tab>
-                  <Tab className= 'cumrap__li'><p>Day laf Tab 3</p></Tab>
-                </TabList>
-                <Rap/>
-              </Tabs>
-              </TabPanel>
-            </div>
-            <div className="cumrap__detail"><Rap/></div>
+      <TabProvider defaultTab="custom-tab-one">
+        <section className="my-custom-tabs">
+          <TabList className="my-custom-tablist">
+            <Tab tabFor="custom-tab-one">Tab 1</Tab>
+            <span className="divider" role="presentation" aria-hidden="true">•</span>
+            <Tab tabFor="custom-tab-two">Tab 2</Tab>
+            <span className="divider" role="presentation" aria-hidden="true">•</span>
+            <Tab tabFor="custom-tab-three" className="my-custom-tab">Tab 3</Tab>
+          </TabList>
+          <div className="my-tabs-panel-wrapper">
+            <TabPanel tabId="custom-tab-one">
+              <p>Tab 1 content</p>
+            </TabPanel>
+            <TabPanel tabId="custom-tab-two">
+              <p>Tab 2 content</p>
+            </TabPanel>
+            <TabPanel tabId="custom-tab-three">
+              <p>Tab 3 content</p>
+            </TabPanel>
           </div>
-          {/* End-CAP 2 */}
-
-        </Tabs>
+        </section>
+      </TabProvider>
       </div>
     </section>
   );
